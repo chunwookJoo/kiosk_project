6ec7d951d0748ff0faddf4aa196b97729a81819f
Joo <jha970107@gmail.com>
2020-11-04 17:49:57 +0900

    model

--- kioskdb/models.py
+++ kioskdb/models.py
@@ -3,24 +3,24 @@ from django.db import models
 
 # Create your models here.
 class customer_bill(models.Model):
-    order_id = models.IntegerField(max_length=50)
-    order_time = models.DateTimeField(max_length=100)
+    order_id = models.IntegerField()
+    order_time = models.DateTimeField()
     goods_name = models.CharField(max_length=50)
-    goods_price = models.IntegerField(max_length=50)
-    goods_count = models.IntegerField(max_length=50)
-    payment = models.IntegerField(max_length=50)
+    goods_price = models.IntegerField()
+    goods_count = models.IntegerField()
+    payment = models.IntegerField()
     payment_method = models.CharField(max_length=50)
 
 
 class order_bill(models.Model):
-    order_id = models.IntegerField(max_length=50)
-    order_time = models.DateTimeField(max_length=100)
+    order_id = models.IntegerField()
+    order_time = models.DateTimeField()
     goods_id = models.CharField(max_length=50)
     goods_name = models.CharField(max_length=50)
-    goods_count = models.IntegerField(max_length=50)
+    goods_count = models.IntegerField()
 
 
-class Contens(models.Model):
+class contents(models.Model):
     goods_name = models.CharField(max_length=50)
-    goods_price = models.IntegerField(max_length=50)
+    goods_price = models.IntegerField()
     goods_origin = models.CharField(max_length=50)
