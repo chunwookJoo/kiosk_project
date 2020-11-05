d3b1f158e1796fb9235d7a0e661411f7c98d6dc7
Roddyd94 <pky941129@gmail.com>
2020-11-05 14:41:27 +0900

    db admin registered

--- kioskdb/admin.py
+++ kioskdb/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import customer_bill, order_bill, contents
 
+admin.site.register(customer_bill)
+admin.site.register(order_bill)
+admin.site.register(contents)
 # Register your models here.
